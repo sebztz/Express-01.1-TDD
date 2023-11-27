27188557d3fc197874b9e67f6c3307fd4d47dcad
sebastien_lacour <sebastien-lacour@hotmail.com>
2023-11-27 15:54:08 +0100

    done

--- app.js
+++ app.js
@@ -1,59 +1,56 @@
 const express = require("express");
-
 const app = express();
 
-const port = 5000;
+const welcome = (req, res) => {
+  res.send("Welcome to my favourite movie list");
+};
+
+app.get("/", welcome);
 
 const movies = [
-  {
-    id: 1,
-    title: "Citizen Kane",
-    director: "Orson Wells",
-    year: "1941",
-    color: false,
-    duration: 120,
-  },
-  {
-    id: 2,
-    title: "The Godfather",
-    director: "Francis Ford Coppola",
-    year: "1972",
-    color: true,
-    duration: 180,
-  },
-  {
-    id: 3,
-    title: "Pulp Fiction",
-    director: "Quentin Tarantino",
-    year: "1994",
-    color: true,
-    duration: 180,
-  },
-];
-
-app.get("/", (req, res) => {
-  "Welcome to my favourite movie list";
-});
-
-app.get("api/movies", (req, res) => {
-  res.sendStatus(200).json(movies);
-});
-app.get("/api/movies/:id", (req, res) => {
-  const movieId = parseInt(req.params.id);
-  const movie = movies.find((movie) => movie.id === movieId);
-
-  if (movie) {
-    res.status(200).json(movie);
-  } else {
-    res.status(404).send("Not Found");
-  }
-});
-
-
-app.listen(port, (err) => {
-  if (err) {
-    console.error("Something bad happened");
-  } else {
-    console.log(`Server is listening on ${port}`);
-  }
-});
+    {
+      id: 1,
+      title: "Citizen Kane",
+      director: "Orson Wells",
+      year: "1941",
+      color: false,
+      duration: 120,
+    },
+    {
+      id: 2,
+      title: "The Godfather",
+      director: "Francis Ford Coppola",
+      year: "1972",
+      color: true,
+      duration: 180,
+    },
+    {
+      id: 3,
+      title: "Pulp Fiction",
+      director: "Quentin Tarantino",
+      year: "1994",
+      color: true,
+      duration: 180,
+    },
+  ];
+  const getMovies = (req, res) => {
+    res.json(movies);
+  };
+  
+  app.get("/api/movies", getMovies);
+  
+  const getMovieById = (req, res) => {
+    const id = parseInt(req.params.id);
+  
+    const movie = movies.find((movie) => movie.id === id);
+  
+    if (movie != null) {
+      res.json(movie);
+    } else {
+      res.sendStatus(404);
+    }
+  };
+  
+  app.get("/api/movies/:id", getMovieById);
+  
+  module.exports = app;
